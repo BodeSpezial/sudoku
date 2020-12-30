c2c08bb67887f26a663bdd1ecd8ae47df356b64e
BodeSpezial <konstantin.bode@outlook.com>
2020-12-30 19:49:20 +0100

    add function to display sudoku

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -1,3 +1,29 @@
+#include <iostream>
+
+using namespace std;
+
+void printSdk(){
+    printf("-----------------------------------------\n");
+    // TODO: Refractor to loops
+    // 3 times the following 4 lines
+    // 3 times the || %i | %i | %i pattern 
+    //
+    printf("|| %i | %i | %i || %i | %i | %i || %i | %i | %i ||\n",8,8,8,8,8,8,8,8,8);
+    printf("|| %i | %i | %i || %i | %i | %i || %i | %i | %i ||\n",8,8,8,8,8,8,8,8,8);
+    printf("|| %i | %i | %i || %i | %i | %i || %i | %i | %i ||\n",8,8,8,8,8,8,8,8,8);
+    printf("-----------------------------------------\n");
+    printf("|| %i | %i | %i || %i | %i | %i || %i | %i | %i ||\n",8,8,8,8,8,8,8,8,8);
+    printf("|| %i | %i | %i || %i | %i | %i || %i | %i | %i ||\n",8,8,8,8,8,8,8,8,8);
+    printf("|| %i | %i | %i || %i | %i | %i || %i | %i | %i ||\n",8,8,8,8,8,8,8,8,8);
+    printf("-----------------------------------------\n");
+    printf("|| %i | %i | %i || %i | %i | %i || %i | %i | %i ||\n",8,8,8,8,8,8,8,8,8);
+    printf("|| %i | %i | %i || %i | %i | %i || %i | %i | %i ||\n",8,8,8,8,8,8,8,8,8);
+    printf("|| %i | %i | %i || %i | %i | %i || %i | %i | %i ||\n",8,8,8,8,8,8,8,8,8);
+    printf("-----------------------------------------\n");
+}
+
 int main() {
+    printSdk();
+    
     return 0;
 }
